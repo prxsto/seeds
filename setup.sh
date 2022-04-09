a5e5b14ea8519ff2712ca14d1d13b6e19b1f2729
preston pape <papepreston@gmail.com>
2022-04-08 19:04:17 -0700

    setup script update

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 mkdir -p ~/.streamlit/
-echo "[theme]" > ~/.streamlit/config.toml
-echo "base = 'light'" > ~/.streamlit/config.toml
-echo "primaryColor = '#16B64E'" > ~/.streamlit/config.toml
-echo "[server]" > ~/.streamlit/config.toml
-echo "headless = true" > ~/.streamlit/config.toml
-echo "port = $PORT" > ~/.streamlit/config.toml
-echo "enableCORS = false" > ~/.streamlit/config.toml
+echo "[theme]
+base = 'light'
+primaryColor = '#16B64E'
+[server]
+headless = true
+port = $PORT
+echo enableCORS = false
+" > ~/.streamlit/config.toml
