ba40bc625ef4da0675affe700c424e9eecc591ed
prxsto <15711032+prxsto@users.noreply.github.com>
2024-07-31 00:51:27 -0700

    test

--- web_tool.py
+++ web_tool.py
@@ -692,7 +692,7 @@ def web_tool(model):
 
         if count > 0:
 
-            st.text_area(label="results",value=st.session_state.results)
+            print(st.session_state.results)
             fig = plot_scatter(
                 st.session_state.results[plotd[x_axis_data]],
                 st.session_state.results[plotd[y_axis_data]],
